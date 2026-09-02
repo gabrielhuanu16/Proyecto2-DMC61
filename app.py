bfc734ca4ce59e978de395f6bf4232493c00f692
gabrielhuanu16 <gabrielhuanu16@gmail.com>
2026-09-02 18:41:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.sidebar.title("Secciones")
+st.sidebar.image("DMC_logo.png", width=100)
+modulo = st.sidebar.selectbox("Seleccione un módulo", ["Home", "Carga del Dataset"])
+if modulo == "Home":
